6df5cf648caa143926b5dd0d82ae3e1477a226a4
suap-dev <suap.dev@gmail.com>
2023-07-07 01:27:23 +0200

    read and log mouse click position

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod engine;
 
 use engine::{Entity, World};
 use glium::glutin::{
-    event,
+    dpi::PhysicalPosition,
+    event::{self, ElementState},
     event_loop::{ControlFlow, EventLoop},
 };
 use nalgebra_glm::vec2;
@@ -24,12 +25,36 @@ fn main() {
     let rectangle = world.add(rectangle);
     let triangle = world.add(triangle);
 
+
+    let mut mouse_position = PhysicalPosition::new(-1.0, -1.0);
     event_loop.run(move |event, _, control_flow| {
         world.translate_citizen(rectangle, vec2(0.00006, 0.0));
         world.render();
         if let event::Event::WindowEvent { event, .. } = event {
-            if let event::WindowEvent::CloseRequested = event {
-                *control_flow = ControlFlow::Exit;
+            match event {
+                event::WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                event::WindowEvent::CursorMoved {
+                    device_id: _,
+                    position,
+                    modifiers: _,
+                } => {
+                    mouse_position = position;
+                }
+                event::WindowEvent::MouseInput {
+                    device_id: _,
+                    state,
+                    button,
+                    modifiers: _,
+                } => {
+                    println!("Mouse:");
+                    println!(" - button: {:?}", button);
+                    println!(" - state: {:?}", state);
+                    println!(" - position: {:?}", mouse_position);
+                    println!();
+                }
+                _ => {}
             }
         }
     });
