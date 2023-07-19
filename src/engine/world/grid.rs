f079fd6b9249c87c0e9c56875c46ed9739cf62e0
suap-dev <suap.dev@gmail.com>
2023-07-20 01:05:05 +0200

    grid struct

--- src/engine/world/grid.rs
+++ src/engine/world/grid.rs
@@ -2,7 +2,7 @@
 pub struct Grid {
     dimensions: usize,
     radius: f32,
-    pockets: Vec<Vec<Vec<usize>>>,
+    pub pockets: Vec<Vec<Vec<usize>>>,
 }
 // TODO: rozpisać na kartce jak to właściwie ma działać, jak konwertowac, jakie są granice każdej komórki..
 // czy będzie trzeba zupełnie przepisać wszystko by ten grid obsłużyć?
@@ -17,12 +17,29 @@ impl Grid {
         }
     }
 
+    pub fn bogus() -> Self {
+        Self {
+            dimensions: 0,
+            radius: 0.0,
+            pockets: vec![vec![vec![]; 0]; 0],
+        }
+    }
+
     pub fn push(&mut self, idx: usize, x: f32, y: f32) {
         let (i, j) = self.get_ij(x, y);
 
-        let x = self.pockets.get_mut(i).unwrap();
-        let y = x.get_mut(j).unwrap();
-        y.push(idx);
+        let column = self.pockets.get_mut(i).unwrap();
+        let row = column.get_mut(j).unwrap();
+
+        row.push(idx);
+    }
+
+    pub fn clear(&mut self) {
+        for v_v in &mut self.pockets {
+            for v in v_v {
+                v.clear();
+            }
+        }
     }
 
     pub fn log_dim(&self) {
